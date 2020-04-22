c943c75520c1a4c4e95dfb38af69d097e9c2e930
Gustav Burchardt <gustavburchardt@gmail.com>
2020-04-22 12:52:50 +0200

    Implement slider

---
+++ src/artists/artists.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function () {
+  let activeSlide = 0
+
+  function setActiveSlide (n) {
+    document.getElementById('artist-' + activeSlide).classList.remove('active')
+    activeSlide = n
+    document.getElementById('artist-' + activeSlide).classList.add('active')
+  }
+
+  const leftButton = document.getElementById('artist-track-left')
+  const rightButton = document.getElementById('artist-track-right')
+
+  const items = document.getElementById('artists').getElementsByClassName('artist').length
+
+  leftButton.addEventListener('click', function () {
+    setActiveSlide((activeSlide + items - 1) % items)
+  })
+  rightButton.addEventListener('click', function () {
+    setActiveSlide((activeSlide + 1) % items)
+  })
+
+  setActiveSlide(activeSlide)
+})
