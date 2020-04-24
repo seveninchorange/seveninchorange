37310a2a2f7d0f90522d6b2b8a5f21caf0f97b0e
Gustav Burchardt <gustavburchardt@gmail.com>
2020-04-24 09:44:31 +0200

    Refactor scroll nativation

--- src/script.js
+++ src/script.js
@@ -1,29 +1,37 @@
-let id = 0
+window.addEventListener('load', function () {
+  let id = 0
 
-function increment (y, handlerId) {
-  if (id !== handlerId) {
-    return
+  function increment (y, handlerId) {
+    if (id !== handlerId) {
+      return
+    }
+
+    if (Math.abs(window.scrollY - y) < 1) {
+      window.scrollTo(0, y)
+      return
+    }
+
+    window.scrollBy(0, Math.max(1, (y - window.scrollY) / 15))
+
+    setTimeout(() => increment(y, handlerId), 10)
   }
 
-  if (Math.abs(window.scrollY - y) < 1) {
-    window.scrollTo(0, y)
-    return
+  function smoothScrollTo (elId) {
+    const el = document.getElementById(elId)
+    if (el) {
+      const y = el.getBoundingClientRect().y + window.scrollY
+
+      increment(y, id)
+    }
   }
 
-  window.scrollBy(0, Math.max(1, (y - window.scrollY) / 15))
+  window.addEventListener('wheel', function () {
+    id++
+  })
 
-  setTimeout(() => increment(y, handlerId), 10)
-}
-
-window.smoothScrollTo = function smoothScrollTo (elId) {
-  const el = document.getElementById(elId)
-  if (el) {
-    const y = el.getBoundingClientRect().y + window.scrollY
-
-    increment(y, id)
+  const navigationElements = document.querySelectorAll('[data-navigate-to]')
+  for (let i = 0; i < navigationElements.length; i++) {
+    const el = navigationElements[i]
+    el.addEventListener('click', () => smoothScrollTo(el.dataset.navigateTo))
   }
-}
-
-window.addEventListener('wheel', function () {
-  id++
 })
